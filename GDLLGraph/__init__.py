6fd38fe10f69e4ab7052503cfe03822d2ffbb49e
MonkeyDisney <kijin19@gmail.com>
2022-12-18 14:45:05 +0900

    make samplers updates

---
+++ GDLLGraph/__init__.py
@@ -0,0 +1,3 @@
+from .GDLLGraph import *
+
+__version__ = "1.0.1"
--- graphSampler/example.py
+++ graphSampler/example.py
@@ -1,1 +1,26 @@
-import GDLLGraph.GDLLGraph
+from GDLLGraph.GDLLGraph import *
+import time
+import argparse
+import numpy as np
+import math
+import tqdm
+import networkx
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+from GNN.utils.utils import load_data, load_khop, accuracy
+from GNN.gnn.Convolution import gcnConv
+
+import samplers
+
+data_dir = "../Graph-Embedding/src/data/cora"
+dataset = "cora.cites"
+dataset_features = "cora.content"
+g = GDLLGraph(data_dir, dataset, dataset_features, sep = "\t" )
+print(g.node_data)
+
+fanouts = "10,10,10"
+
+# sampler = samplers.NeighborSamplerByNode(g, fanouts, edge_dir="in")
