06bc328da900e31edccd45448a9c42a8ea084a94
sahibzada-irfanullah <sahibzada.iu@gmail.com>
2022-12-18 18:15:06 +0900

    code cleaning

--- Graph-Embedding/src/ge/main_ge.py
+++ Graph-Embedding/src/ge/main_ge.py
@@ -1,8 +1,6 @@
 
 import sys
 sys.path.append("..")
-import time
-import datetime
 from utils import saveEmbedding
 from deepWalk import DeepWalk
 from node2vec import Node2vec
@@ -11,7 +9,7 @@ from GDLLGraph import GDLLGraph
 from skipgram import SkipGramModel
 
 
-script_start = time.time()
+
 # Set Path to data
 # dataset = "citeseer - Copy"
 # dataset = "cora.cites"
@@ -26,12 +24,11 @@ embedding_dir = "../data"
 
 # Load graph
 
-myGraph = GDLLGraph(data_dir, graph, graph_features = graph_features, sep= "\t")
-myGraph = myGraph.g
+myGraph = GDLLGraph(data_dir, graph, graph_features = graph_features, sep= "\t").g
 
 # Set Parameters
-embedDim = 3 # embedding size
-numbOfWalksPerVertex = 3 # walks per vertex
+embedDim = 5 # embedding size
+numbOfWalksPerVertex = 2 # walks per vertex
 walkLength = 3# walk length
 lr =0.25 # learning rate
 windowSize = 3 # window size
@@ -66,17 +63,16 @@ print("Saving embedding to disk")
 saveEmbedding(data_dir, graph_features, embedding_dir, rw)
 #
 # print("Generating embedding for a node and edge")
-node1 = 35
-node2 = 36
-# Get Embedding for a node
-emb = rw.getNodeEmbedding(node1)
-print("Node Embedding", emb.data)
+# node1 = 35
+# node2 = 36
+# # Get Embedding for a node
+# emb = rw.getNodeEmbedding(node1)
+# print("Node Embedding", emb.data)
 # #
 # # # Get Embedding for an edge
 # emb = rw.getEdgeEmbedding(node1, node2)
 # print("Edge Embedding", emb.data)
-Total_time = time.time() - script_start
-print("\nTotal time for script completion :" + str(datetime.timedelta(seconds=int(Total_time))))
+
 
 
 
