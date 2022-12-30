40258da1c1f0c4942e624d90399054c4408a8d46
KijinKHU <94345294+KijinKHU@users.noreply.github.com>
2022-12-30 17:27:02 +0900

    Common sampler_utils.py

--- graphSampler/example.py
+++ graphSampler/example.py
@@ -4,11 +4,14 @@ import argparse
 import numpy as np
 import math
 import tqdm
-import networkx as nx
+import networkx
 
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
+
+from GNN.utils.utils import load_data, load_khop, accuracy
+from GNN.gnn.Convolution import gcnConv
 
 import samplers
 
@@ -16,27 +19,8 @@ data_dir = "../Graph-Embedding/src/data/cora"
 dataset = "cora.cites"
 dataset_features = "cora.content"
 g = GDLLGraph(data_dir, dataset, dataset_features, sep = "\t" )
-# print("node_data: ",g.node_data)
-# neiborsOfOneNode = g.GetNeighbors(31336)
-# print(type(neiborsOfOneNode))
-fanout = [3,3]
-example_seed_nodes = [('31336'),('1129442')]
+print(g.node_data)
 
-sampler = samplers.NeighborSamplerByNode(fanout)
-MFGs = sampler.sample(g,example_seed_nodes)
-print(MFGs)
+fanouts = "10,10,10"
 
-
-
-# sampled MFGs result
-# [
-# [['31336', '31336', '31336', '1129442', '1129442', '1129442'],
-# ['31353', '686532', '686532', '31353', '43698', '31353'],
-# <networkx.classes.graph.Graph object at 0x0000022E9DB09310>],
-#
-# [['31353', '31353', '31353', '686532', '686532', '686532', '686532', '686532',
-# '686532', '31353', '31353', '31353', '43698', '43698', '43698', '31353', '31353', '31353'],
-# ['31336', '1124844', '1123576', '31349', '31353', '43698', '31353', '31353',
-# '31349', '194617', '1063773', '31349', '31349', '10531', '31349', '1129442', '686532', '286562'],
-# <networkx.classes.graph.Graph object at 0x0000022E9DB09370>]
-# ]
+# sampler = samplers.NeighborSamplerByNode(g, fanouts, edge_dir="in")
